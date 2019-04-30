b1d188624e72e6ff91437a74d052af41b65c07c0
mayabytatech <maya.k.k@lxisoft.com>
2019-04-30 14:58:00 +0530

    ip address change factored

--- src/main/java/com/bytatech/ayoos/client/doctor/api/ReservedSlotResourceApiClient.java
+++ src/main/java/com/bytatech/ayoos/client/doctor/api/ReservedSlotResourceApiClient.java
@@ -3,6 +3,6 @@ package com.bytatech.ayoos.client.doctor.api;
 import org.springframework.cloud.openfeign.FeignClient;
 import com.bytatech.ayoos.client.doctor.ClientConfiguration;
 
-@FeignClient(name="${doctor.name:doctor}", url="${doctor.url:35.231.213.38:8083/}", configuration = ClientConfiguration.class)
+@FeignClient(name="${doctor.name:doctor}", url="${doctor.url:34.73.191.107:8083/}", configuration = ClientConfiguration.class)
 public interface ReservedSlotResourceApiClient extends ReservedSlotResourceApi {
 }
