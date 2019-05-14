c006481dc07ddf2f6854120e2bbfd3033fde4dae
mayabytatech <maya.k.k@lxisoft.com>
2019-05-14 14:40:30 +0530

    import changed

--- src/main/java/com/bytatech/ayoos/client/consultation/api/UserResourceApiClient.java
+++ src/main/java/com/bytatech/ayoos/client/consultation/api/UserResourceApiClient.java
@@ -1,7 +1,7 @@
 package com.bytatech.ayoos.client.consultation.api;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import com.bytatech.ayoos.client.consultation.ClientConfiguration;
+import com.bytatech.ayoos.client.doctor.ClientConfiguration;
 
 @FeignClient(name="${consultation.name:consultation}", url="${consultation.url:35.243.137.225:8001/}", configuration = ClientConfiguration.class)
 public interface UserResourceApiClient extends UserResourceApi {
