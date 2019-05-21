5e6731a025f63ca71425d04fba9a0a3bb8b90a69
rafeekbytatech <abdul.rafeek@lxisoft.com>
2019-05-21 09:43:48 +0530

    create DoctorSettings and PaymentSettings

--- src/main/java/com/bytatech/ayoos/web/rest/CommandResource.java
+++ src/main/java/com/bytatech/ayoos/web/rest/CommandResource.java
@@ -1,18 +1,18 @@
- /*
- * Copyright 2002-2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/*
+* Copyright 2002-2016 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package com.bytatech.ayoos.web.rest;
 
 import static org.elasticsearch.index.query.QueryBuilders.termQuery;
@@ -56,123 +56,142 @@ import com.bytatech.ayoos.web.rest.errors.BadRequestAlertException;
 import com.bytatech.ayoos.web.rest.util.HeaderUtil;
 
 /**
- * TODO Provide a detailed description here 
- * @author MayaSanjeev
- * mayabytatech, maya.k.k@lxisoft.com
+ * TODO Provide a detailed description here
+ * 
+ * @author MayaSanjeev mayabytatech, maya.k.k@lxisoft.com
  */
 @RestController
 @RequestMapping("/api/commands")
 public class CommandResource {
-	
-	 @PostMapping("/doc")
+
+	@PostMapping("/doc")
 	public String test() {
 		return "success";
 	}
-	 
-	  @Autowired
-	    DoctorResourceApi doctorApi;
-	  
-	  @Autowired
-	  QualificationResourceApi qualificationApi;
-	  
-	  @Autowired
-	  ContactInfoResourceApi contactInfoResourceApi;
-	  
-	  @Autowired
-	  SessionInfoResourceApi sessionInfoAPi;
-	  
-	  @Autowired
-	  WorkPlaceResourceApi workPlaceResourceApi;
-	  
-	  @Autowired
-	  ReservedSlotResourceApi reservedSlotResourceApi;
-	  
-	  @Autowired
-	  ConsultationCommandResourceApi consultationApi;
-	  
-	  @Autowired
-	  ConsultationQueryResourceApi consultationQueryApi;
-	  
-	  @Autowired
-	  AppointmentCommandResourceApi appointmentCommandResourceApi;
-	  
-	  @PostMapping("/doctors")
-	    public ResponseEntity<DoctorDTO> createDoctor(@RequestBody DoctorDTO doctorDTO) throws URISyntaxException {
-		  
-		  return  doctorApi.createDoctorUsingPOST(doctorDTO);
-	  }
-	  
-	  @PutMapping("/doctors")
-	    public ResponseEntity<DoctorDTO> updateDoctor(@RequestBody DoctorDTO doctorDTO)  {
-		  return doctorApi.updateDoctorUsingPUT(doctorDTO);
-	  }    
-	  
-	  @PostMapping("/contact-infos")
-	  public ResponseEntity<ContactInfoDTO> createContactInfo(@RequestBody ContactInfoDTO contactInfoDTO){
-		  
-		  ResponseEntity<ContactInfoDTO> contactInfo =contactInfoResourceApi.createContactInfoUsingPOST(contactInfoDTO);
-		  
-		  return contactInfo;
-	  }
-	  
-	  @PutMapping("/contact-infos")
-	  public ResponseEntity<ContactInfoDTO> updateContactInfo(@RequestBody ContactInfoDTO contactInfoDTO){
-		  return contactInfoResourceApi.updateContactInfoUsingPUT(contactInfoDTO);
-	  }
-	 
-	  @DeleteMapping("/contact-infos/{id}")
-	  public void deleteContactInfo(@PathVariable Long id){
-		  contactInfoResourceApi.deleteContactInfoUsingDELETE(id);
-	  }
-	  
-	  @PostMapping("/qualifications")
-	  public ResponseEntity<QualificationDTO> createQualification(@RequestBody QualificationDTO qualificationDTO){
-		 return qualificationApi.createQualificationUsingPOST(qualificationDTO);
-	  }
-	  @PutMapping("/qualifications")
-	  public ResponseEntity<QualificationDTO> updateQualification(@RequestBody QualificationDTO qualificationDTO){
-		 return qualificationApi.updateQualificationUsingPUT(qualificationDTO);
-	  }
-	  
-	  @DeleteMapping("/qualifications/{id}")
-	  public void deleteQualification(@PathVariable Long id){
-		  qualificationApi.deleteQualificationUsingDELETE(id);
-	  }
-	  
-	  @PostMapping("/work-places")
-	  public ResponseEntity<WorkPlaceDTO> createWorkPlace(@RequestBody WorkPlaceDTO workPlaceDTO){
-		  ResponseEntity<WorkPlaceDTO> workPlace=  workPlaceResourceApi.createWorkPlaceUsingPOST(workPlaceDTO);
-		
-		  return workPlace;
-	  }
-	  
-	  @PutMapping("/work-places")
-	  public ResponseEntity<WorkPlaceDTO> updateWorkPlace(@RequestBody WorkPlaceDTO workPlaceDTO){
-		  
-		  return workPlaceResourceApi.updateWorkPlaceUsingPUT(workPlaceDTO);
-		  
-	  }
-	  
-	  @DeleteMapping("/work-places")
-	  public void deleteWorkPlace(@PathVariable Long id){
-		  workPlaceResourceApi.deleteWorkPlaceUsingDELETE(id);
-	  }
-	  
-	  @PostMapping("/sessionInfo")
-	 public ResponseEntity<List<SessionInfoDTO>> createSessionInfo(@RequestBody List<SessionInfoDTO> sessionInfoDTO,
-				@RequestParam List<Integer> monthList){
-		return  sessionInfoAPi.setSessionToMonthUsingPOST(monthList, sessionInfoDTO);
-	 }
-	 
-	  @PostMapping("/slot/{date}/{doctorId}")
-	  public ResponseEntity<List<ReservedSlotDTO>> createSlot(@PathVariable String date,@PathVariable Long doctorId){
-		 return  reservedSlotResourceApi.createSlotUsingGET(date,doctorId);
-	  }
+
+	@Autowired
+	DoctorResourceApi doctorApi;
+
+	@Autowired
+	QualificationResourceApi qualificationApi;
+
+	@Autowired
+	ContactInfoResourceApi contactInfoResourceApi;
+
+	@Autowired
+	SessionInfoResourceApi sessionInfoAPi;
+
+	@Autowired
+	WorkPlaceResourceApi workPlaceResourceApi;
+
+	@Autowired
+	ReservedSlotResourceApi reservedSlotResourceApi;
+
+	@Autowired
+	ConsultationCommandResourceApi consultationApi;
+
+	@Autowired
+	ConsultationQueryResourceApi consultationQueryApi;
+
+	@Autowired
+	AppointmentCommandResourceApi appointmentCommandResourceApi;
+	@Autowired
+	PaymentSettingsResourceApi paymentSettingsResourceApi;
+
+	@Autowired
+	DoctorSettingsResourceApi doctorSettingsResourceApi;
+
+	@PostMapping("/doctors")
+	public ResponseEntity<DoctorDTO> createDoctor(@RequestBody DoctorDTO doctorDTO) throws URISyntaxException {
+
+		return doctorApi.createDoctorUsingPOST(doctorDTO);
+	}
+
+	@PutMapping("/doctors")
+	public ResponseEntity<DoctorDTO> updateDoctor(@RequestBody DoctorDTO doctorDTO) {
+		return doctorApi.updateDoctorUsingPUT(doctorDTO);
+	}
+
+	@PostMapping("/contact-infos")
+	public ResponseEntity<ContactInfoDTO> createContactInfo(@RequestBody ContactInfoDTO contactInfoDTO) {
+
+		ResponseEntity<ContactInfoDTO> contactInfo = contactInfoResourceApi.createContactInfoUsingPOST(contactInfoDTO);
+
+		return contactInfo;
+	}
+
+	@PutMapping("/contact-infos")
+	public ResponseEntity<ContactInfoDTO> updateContactInfo(@RequestBody ContactInfoDTO contactInfoDTO) {
+		return contactInfoResourceApi.updateContactInfoUsingPUT(contactInfoDTO);
+	}
+
+	@DeleteMapping("/contact-infos/{id}")
+	public void deleteContactInfo(@PathVariable Long id) {
+		contactInfoResourceApi.deleteContactInfoUsingDELETE(id);
+	}
+
+	@PostMapping("/qualifications")
+	public ResponseEntity<QualificationDTO> createQualification(@RequestBody QualificationDTO qualificationDTO) {
+		return qualificationApi.createQualificationUsingPOST(qualificationDTO);
+	}
+
+	@PutMapping("/qualifications")
+	public ResponseEntity<QualificationDTO> updateQualification(@RequestBody QualificationDTO qualificationDTO) {
+		return qualificationApi.updateQualificationUsingPUT(qualificationDTO);
+	}
+
+	@DeleteMapping("/qualifications/{id}")
+	public void deleteQualification(@PathVariable Long id) {
+		qualificationApi.deleteQualificationUsingDELETE(id);
+	}
+
+	@PostMapping("/work-places")
+	public ResponseEntity<WorkPlaceDTO> createWorkPlace(@RequestBody WorkPlaceDTO workPlaceDTO) {
+		ResponseEntity<WorkPlaceDTO> workPlace = workPlaceResourceApi.createWorkPlaceUsingPOST(workPlaceDTO);
+
+		return workPlace;
+	}
+
+	@PutMapping("/work-places")
+	public ResponseEntity<WorkPlaceDTO> updateWorkPlace(@RequestBody WorkPlaceDTO workPlaceDTO) {
+
+		return workPlaceResourceApi.updateWorkPlaceUsingPUT(workPlaceDTO);
+
+	}
+
+	@DeleteMapping("/work-places")
+	public void deleteWorkPlace(@PathVariable Long id) {
+		workPlaceResourceApi.deleteWorkPlaceUsingDELETE(id);
+	}
+
+	@PostMapping("/sessionInfo")
+	public ResponseEntity<List<SessionInfoDTO>> createSessionInfo(@RequestBody List<SessionInfoDTO> sessionInfoDTO,
+			@RequestParam List<Integer> monthList) {
+		return sessionInfoAPi.setSessionToMonthUsingPOST(monthList, sessionInfoDTO);
+	}
+
+	@PostMapping("/slot/{date}/{doctorId}")
+	public ResponseEntity<List<ReservedSlotDTO>> createSlot(@PathVariable String date, @PathVariable Long doctorId) {
+		return reservedSlotResourceApi.createSlotUsingGET(date, doctorId);
+	}
+
+	@PostMapping("/paymentSettings")
+	public ResponseEntity<PaymentSettingsDTO> createPaymentSetting(@RequestBody PaymentSettingsDTO paymentSettingsDTO) {
+		return paymentSettingsResourceApi.createPaymentSettingsUsingPOST(paymentSettingsDTO);
+	}
+
 	
-	  
- ///////////--------------CONSULTATION----------------\\\\\\\\\\\\\\\\\\
-	  
-	  
+	@PostMapping("/doctorSettings")
+	public ResponseEntity<DoctorSettingsDTO> createDoctorSetting(@RequestBody DoctorSettingsDTO doctorSettingsDTO) {
+		return doctorSettingsResourceApi.createDoctorSettingsUsingPOST(doctorSettingsDTO);
+	}
+	
+	
+	
+	
+	
+	/////////// --------------CONSULTATION----------------\\\\\\\\\\\\\\\\\\
+
 	@PostMapping("/initiate-Consultation")
 	public ResponseEntity<String> initiate(@RequestBody InitiateMedicalSummaryRequest medicalSummaryRequest) {
 
@@ -200,20 +219,24 @@ public class CommandResource {
 	}
 
 	@PostMapping("/collect-Prescription-Info/{taskId}")
-	public void collectPrescriptionInformations(@PathVariable String taskId, @RequestBody List<PrescriptionRequest> prescriptionRequest) {
+	public void collectPrescriptionInformations(@PathVariable String taskId,
+			@RequestBody List<PrescriptionRequest> prescriptionRequest) {
 
 		consultationApi.collectPrescriptionInformationsUsingPOST(taskId, prescriptionRequest);
 	}
 
 	@PostMapping("/upload-File")
-	public String uploadPrescription( @RequestParam MultipartFile file){
+	public String uploadPrescription(@RequestParam MultipartFile file) {
 		consultationApi.uploadFileUsingPOST(file);
 		return "success";
 	}
-	//.....................................................Appointment...................................................
+
+	// .....................................................Appointment...................................................
 	@PostMapping("/processAppointmentRequest/{taskId}")
-	public ResponseEntity<com.bytatech.ayoos.client.appointment.model.CommandResource> getProcessAppointmentRequest(@PathVariable String taskId,@RequestBody AppointmentConfirmationResponse appointmentConfirmationResponse){
-		return appointmentCommandResourceApi.processAppointmentRequestUsingPOST(taskId, appointmentConfirmationResponse);
+	public ResponseEntity<com.bytatech.ayoos.client.appointment.model.CommandResource> getProcessAppointmentRequest(
+			@PathVariable String taskId, @RequestBody AppointmentConfirmationResponse appointmentConfirmationResponse) {
+		return appointmentCommandResourceApi.processAppointmentRequestUsingPOST(taskId,
+				appointmentConfirmationResponse);
 	}
-	
+
 }
