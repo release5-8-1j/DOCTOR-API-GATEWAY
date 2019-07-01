39a11354a7e05eceea83fc2517b8dd57a1b5a237
mayabytatech <maya.k.k@lxisoft.com>
2019-07-01 09:31:51 +0530

    swaggered the ip cahnges

--- src/main/java/com/bytatech/ayoos/client/consultation/api/DiagnosisResourceApiClient.java
+++ src/main/java/com/bytatech/ayoos/client/consultation/api/DiagnosisResourceApiClient.java
@@ -4,6 +4,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 
 import com.bytatech.ayoos.client.consultation.ConsultationClientConfiguration;
 
-@FeignClient(name="${consultation.name:consultation}", url="${consultation.url:35.243.137.225:8001/}", configuration = ConsultationClientConfiguration.class)
+@FeignClient(name="${consultation.name:consultation}", url="${consultation.url:35.184.100.236:8001/}", configuration = ConsultationClientConfiguration.class)
 public interface DiagnosisResourceApiClient extends DiagnosisResourceApi {
 }
